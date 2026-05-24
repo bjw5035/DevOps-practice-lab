55ae5b8eeef117133b2e319ddfae519727dd8c95
bjw5035 <bjw5035@gmail.com>
2026-05-24 16:51:10 +0900

    feat(terraform): add bastion EC2 instance with SSH key pair

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,3 +129,19 @@ resource "aws_security_group" "bastion" {
     description = "Allow all outbound"
   }
 }
+
+
+resource "aws_instance" "bastion" {
+
+  ami                    = "ami-0533c7c245a0ee297"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_a.id
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+  key_name               = "bastion-key"
+
+  tags = {
+    Name = "bastion_host"
+  }
+
+}
+
