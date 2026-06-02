3ee8ff1d89b1829d43b5f43ab2d5b5dbb2a6210b
bjw5035 <bjw5035@gmail.com>
2026-06-02 23:21:02 +0900

    refactor: parameterize terraform variables

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "bastion" {
 
   ami                    = "ami-0533c7c245a0ee297"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.bastion.id]
-  key_name               = "bastion-key"
+  key_name               = var.key_name
 
   tags = {
     Name = "bastion_host"
